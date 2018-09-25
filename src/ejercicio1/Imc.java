811d6abddbefba6cdcf6ffd1a84fa04053d2f5ae
Ian Ducloux <T-101@PC224>
2018-09-24 19:28:30 -0500

    Solucion 3 clases

---
+++ src/ejercicio1/Imc.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author T-101
+ */
+public class Imc {
+      float calcularImc(Persona p){
+      return p.peso/(p.altura*p.altura);
+    }
+}
---
+++ src/ejercicio1/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author T-101
+ */
+public class Persona {
+    float peso, altura, imc;
+
+    public Persona(float peso, float altura) {
+        this.peso = peso;
+        this.altura = altura;
+    }
+    
+  
+}
