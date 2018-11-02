07a179ff4e3a4741bf23551f1da7fe04ad39c620
iamducloux <>
2018-11-01 19:39:08 -0600

    Examen Practico

---
+++ src/examenpractico/TVentas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenpractico;
+
+/**
+ *
+ * @author T-101
+ */
+public class TVentas extends Empleado implements Nomina, Vacaciones{
+
+    @Override
+    public void depositar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/examenpractico/Tarjeta.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenpractico;
+
+/**
+ *
+ * @author T-101
+ */
+public class Tarjeta {
+    
+    private int numero;
+    private float saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+}
